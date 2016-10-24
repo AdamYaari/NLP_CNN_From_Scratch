2a41f38e7fdf0e83cd90d54961c2023451d54d46
AdamYaari <a.u.yaari@gmail.com>
2016-10-24 13:04:59 +0300

    Update CNNFSWeb.py

--- src/CNNFSWeb.py
+++ src/CNNFSWeb.py
@@ -1,9 +1,11 @@
 """Usage: CNNFromScratch.py [-h H | --host=H] [-p P | --port=P] [-w W | --weights_file_path=W]
+                            [-c C | --config_file_name=C]
 
 Options:
     -h --host=H  host server address [default: localhost]
     -p --port=P  port of host server [default: 8000]
     -w --weights_file_path=W  The absolute name & path for the model's saved weights file [default: ./CNNSavedWeights]
+    -c --config_file_name=C  The absolute name & path for the model's test results output file [default: ./CNNTestResults.txt]
 """
 
 import os
@@ -20,8 +22,9 @@ arguments = docopt.docopt(__doc__)
 host = arguments['--host']
 port = int(arguments['--port'])
 weights_file_path = arguments['--weights_file_path']
+config_file_path = arguments['--config_file_name']
 
-id_to_label_dic = {1: "World", 2: "Sports", 3: "Business", 4: "Sci/Tech"}
+id_to_label_dic = {0: "World", 1: "Sports", 2: "Business", 3: "Sci/Tech"}
 
 
 '''Returns an html format of the login screen with a text box to insert article to be classified and
@@ -54,13 +57,24 @@ def login():
 @post('/CNNFromScratch')
 def do_login():
     article_to_label = request.forms.get('article')
-    if len(article_to_label) < module.MAX_INPUT_SIZE:
-        return "Text is to short to be classified (required minimal length of {})".format(module.MAX_INPUT_SIZE)
     if not os.path.exists(weights_file_path):
         return "No weights file was found, looked for file {}, in directory {}".format(weights_file_path, os.getcwd())
+    if not os.path.exists(config_file_path):
+        return "No configuration file was found, looked for file {}, in directory {}".format(weights_file_path, os.getcwd())
+
+    relu_threshold = None
+    with open(config_file_path, "r") as config_file:
+        for line in config_file:
+            splitted_line = line.split(':')
+            if len(splitted_line) > 2:
+                return "Configuration file does not contain threshold for ReLU activation function"
+            if splitted_line[0].strip() == "thresh":
+                if len(splitted_line) < 2:
+                    return "Configuration file threshold for ReLU activation function is empty"
+                relu_threshold = float(splitted_line[1].strip())
+                break
 
     model = pc.Model()
-    relu_threshold = 0.000001
     module.initialize_cnn_model_weights(model, module.cnn_layers_dimensions, module.dense_layers_dimensions)
     model.load(weights_file_path)
 
